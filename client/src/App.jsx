93d8e01cbbd1f2cf6d14bf51c91e36b19873f09f
bartolomecantiga <cantigaonduty@gmail.com>
2024-01-15 08:38:04 +0800

    create run server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import {BrowserRouter, Routes, Route } from'react-router-dom'
+import Headers from './components/Headers'
 import { Signin, SignUp, About, Profile, Home } from './pages'
 
 const App = () => {
   return (
     <BrowserRouter> 
+      <Headers/>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/sign-in' element={<Signin />} />
