a62602b6eac79ab733390c5de92196672454ccf8
bartolomecantiga <cantigaonduty@gmail.com>
2024-01-16 11:42:59 +0800

    add reduc toolkit

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -1,12 +1,14 @@
 import React, {useState} from 'react'
 import {Link, useNavigate} from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { signInStart, signInSucess, signInFailure } from '../redux/user/userSlice'
 
 const Signin = () => {
   
   const [formData, setFormData] = useState({})
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
+  const {loading, error} = useSelector((state)=> state.user)
   const navigate = useNavigate();
+  const dispatch = useDispatch();
   const handleChange = (e) =>{
     setFormData({
       ...formData,
@@ -17,7 +19,7 @@ const Signin = () => {
   const handleSubmit = async(e) => {
     e.preventDefault();
     try {
-      setLoading(true);
+      dispatch(signInStart())
       const res = await fetch('/api/auth/signin', {
         method: 'POST',
         headers: {
@@ -27,18 +29,14 @@ const Signin = () => {
       });
       const data = await res.json();
       if(data.success === false){
-        console.log(data)
-        setError(data.message);
-        setLoading(false);
+        dispatch(signInFailure(data.message))
         return;
       }
-      setLoading(false);
-      setError(null)
+      dispatch(signInSucess(data))
       navigate('/');
-      console.log(data)
-    } catch(etry){
-      setLoading(false);
-      setError(etry.message)
+    
+    } catch(etry){;
+      dispatch(signInFailure(etry.message))
     }
   }
 
@@ -55,7 +53,7 @@ const Signin = () => {
       <div className='flex gap-2 mt-5'>
         <p>Don'/t have an account?</p>
           <Link to={'/sign-up'}>
-            <span className='text-blue-700'>Sign up</span>
+            <span className='text-blue-700'>Sign in</span>
           </Link>
       </div>
       {error && <p className='text-red-500 mt-5'>{error}</p>}
